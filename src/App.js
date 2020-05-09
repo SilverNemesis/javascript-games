d7341734d54813f5cc98a9bc553fb25a8e48a515
Donald Fries <donald.darklight@gmail.com>
2020-05-09 17:40:43 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,130 @@
+import React from 'react';
+import noise from './games/noise';
+
+const games = [noise];
+
+let gameIndex = 0;
+
+let state = {};
+
+const props = {
+  totalFrameTime: 0,
+  frameCount: 0,
+  ctx: null,
+  deltaTime: 0,
+  width: 0,
+  height: 0,
+  image: null
+};
+
+function App() {
+  const canvasRef = React.useRef(null);
+  const requestRef = React.useRef(null);
+  const previousTimeRef = React.useRef(0);
+
+  React.useEffect(() => {
+    const canvas = canvasRef.current;
+    const rect = canvas.getBoundingClientRect()
+    props.ctx = canvas.getContext('2d');
+    props.width = rect.width * window.devicePixelRatio;
+    props.height = rect.height * window.devicePixelRatio;
+
+    state = games[gameIndex](props);
+
+    const animate = currentTime => {
+      if (previousTimeRef.current !== undefined) {
+        props.deltaTime = currentTime - previousTimeRef.current;
+
+        const rect = canvas.getBoundingClientRect()
+        canvas.width = rect.width * window.devicePixelRatio;
+        canvas.height = rect.height * window.devicePixelRatio;
+
+        if (canvas.width !== props.width || canvas.height !== props.height) {
+          props.totalFrameTime = 0;
+          props.frameCount = 0;
+          props.width = canvas.width;
+          props.height = canvas.height;
+        }
+
+        const t0 = performance.now();
+
+        if (state.update) {
+          state.update(props);
+        }
+
+        if (state.render) {
+          state.render(props);
+        }
+
+        const t1 = performance.now();
+        props.totalFrameTime += t1 - t0;
+        props.frameCount++;
+
+        drawFrameTime(props);
+      }
+
+      previousTimeRef.current = currentTime;
+      requestRef.current = requestAnimationFrame(animate);
+    }
+
+    window.addEventListener('click', handleClick);
+    window.addEventListener('keydown', handleKeyDown);
+    window.addEventListener('keyup', handleKeyUp);
+
+    requestRef.current = requestAnimationFrame(animate);
+    return () => {
+      window.removeEventListener('click', handleClick);
+      window.removeEventListener('keydown', handleKeyDown);
+      window.removeEventListener('keyup', handleKeyUp);
+      cancelAnimationFrame(requestRef.current);
+    }
+  }, [canvasRef]);
+
+  return (
+    <div className="screen">
+      <canvas className="canvas" ref={canvasRef}></canvas>
+    </div>
+  );
+}
+
+function handleClick(event) {
+  if (state.handleClick) {
+    state.handleClick(event);
+  }
+}
+
+function handleKeyDown(event) {
+  if (event.keyCode === 9) {
+    event.preventDefault();
+    gameIndex = (gameIndex + 1) % games.length;
+    state = games[gameIndex](props);
+    props.totalFrameTime = 0;
+    props.frameCount = 0;
+  }
+  else {
+    if (state.handleKeyDown) {
+      state.handleKeyDown(event);
+    }
+  }
+}
+
+function handleKeyUp(event) {
+  if (state.handleKeyUp) {
+    state.handleKeyUp(event);
+  }
+}
+
+function drawFrameTime({ ctx, width, totalFrameTime, frameCount }) {
+  const text = state.name + ' (' + (totalFrameTime / frameCount).toFixed(2) + ' ms)';
+  ctx.fillStyle = 'white';
+  ctx.font = '20px monospace';
+  ctx.textAlign = 'center';
+  ctx.textBaseline = 'top';
+  ctx.shadowOffsetX = 1;
+  ctx.shadowOffsetY = 1;
+  ctx.shadowColor = 'rgba(0.2,0.2,0.2,1.0)';
+  ctx.shadowBlur = 4;
+  ctx.fillText(text, width / 2, 0);
+}
+
+export default App;
---
+++ src/games/noise.js
@@ -0,0 +1,56 @@
+let state = {}
+
+export default function initialize({ ctx, width, height }) {
+  state = {
+    size: 1,
+    width,
+    height,
+    image: ctx.createImageData(width, height)
+  }
+  return {
+    name: 'noise',
+    render,
+    handleKeyDown
+  }
+}
+
+function render({ ctx, width, height }) {
+  if (width !== state.width || height !== state.height) {
+    state.width = width;
+    state.height = height;
+    state.image = ctx.createImageData(width, height);
+  }
+
+  const { size } = state;
+  const buffer = state.image.data;
+
+  for (let y = 0; y < height; y += size) {
+    for (let x = 0; x < width; x += size) {
+      const color = Math.floor(Math.random() * 256);
+      for (let dy = y; dy < Math.min(y + size, height); dy++) {
+        for (let dx = x; dx < Math.min(x + size, width); dx++) {
+          const pos = (dy * width + dx) * 4;
+          buffer[pos] = color;
+          buffer[pos + 1] = color;
+          buffer[pos + 2] = color;
+          buffer[pos + 3] = 255;
+        }
+      }
+    }
+  }
+
+  ctx.putImageData(state.image, 0, 0);
+}
+
+function handleKeyDown(event) {
+  if (event.keyCode === 40) {
+    if (state.size > 1) {
+      state.size--;
+    }
+  } else
+    if (event.keyCode === 38) {
+      if (state.size < 100) {
+        state.size++;
+      }
+    }
+}
