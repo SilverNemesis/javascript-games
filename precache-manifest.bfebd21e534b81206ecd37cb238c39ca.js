8976858c8e06b8398bc24c0c8658685147c0f576
Donald Fries <donald.darklight@gmail.com>
2020-05-09 17:42:31 -0500

    Updates

---
+++ precache-manifest.bfebd21e534b81206ecd37cb238c39ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ab897e2d0cba5bc276b5d5663d88164",
+    "url": "/javascript-games/index.html"
+  },
+  {
+    "revision": "48c13576dfd61af7a9d6",
+    "url": "/javascript-games/static/css/main.bbf6ce9d.chunk.css"
+  },
+  {
+    "revision": "3c209e549228de68e828",
+    "url": "/javascript-games/static/js/2.709e72ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/javascript-games/static/js/2.709e72ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48c13576dfd61af7a9d6",
+    "url": "/javascript-games/static/js/main.43a4e858.chunk.js"
+  },
+  {
+    "revision": "9193be7cc0070a5e8491",
+    "url": "/javascript-games/static/js/runtime-main.43c682df.js"
+  }
+]);
